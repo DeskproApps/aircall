3c6468dc7268806cad5200ead96e3c9540edd05a
Paul <1740195+HappyPaul55@users.noreply.github.com>
2025-05-28 17:34:30 +0100

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry (#28)
    * Integrated Sentry for better error tracking in apps.
    
    * Added necessary Sentry configuration settings.
    
    * Upgraded app dependencies.
    ---------
    
    Co-authored-by: OJ Abba <phavorabba@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
+import './instrument';
 import { StrictMode, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ErrorBoundary } from "react-error-boundary";
 import { DeskproAppProvider, LoadingSpinner } from "@deskpro/app-sdk";
 import { queryClient } from "@/query";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 import "./main.css";
 import { App } from "./App";
+import { ErrorBoundary, reactErrorHandler } from '@sentry/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
+  onRecoverableError: reactErrorHandler(),
+});
 root.render((
   <StrictMode>
     <DeskproAppProvider>
