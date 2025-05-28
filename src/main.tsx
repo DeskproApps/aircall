9b966df4fd76bfd6c808fcdd46c0f297ac266a91
Paul Hutchinson <paulhappyhutchinson@protonmail.ch>
2025-05-28 11:19:54 +0000

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import * as Sentry from '@sentry/react';
+import './instrument';
 import { StrictMode, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ErrorBoundary } from "react-error-boundary";
 import { DeskproAppProvider, LoadingSpinner } from "@deskpro/app-sdk";
 import { queryClient } from "@/query";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
@@ -10,16 +11,18 @@ import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 import "./main.css";
 import { App } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
+  onRecoverableError: Sentry.reactErrorHandler(),
+});
 root.render((
   <StrictMode>
     <DeskproAppProvider>
       <HashRouter>
         <QueryClientProvider client={queryClient}>
           <Suspense fallback={<LoadingSpinner/>}>
-            <ErrorBoundary fallback={<>here was an error!</>}>
+            <Sentry.ErrorBoundary fallback={<>here was an error!</>}>
               <App />
-            </ErrorBoundary>
+            </Sentry.ErrorBoundary>
           </Suspense>
         </QueryClientProvider>
       </HashRouter>
