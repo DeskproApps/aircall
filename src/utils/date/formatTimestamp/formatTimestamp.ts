df53ef82e62fb0d8c9ec12dd92883518d4452f9a
OJ Abba <phavorabba@gmail.com>
2025-06-05 17:37:34 +0100

    [Bug] Fix Timezone issue in `formatTimestamp`

--- src/utils/date/formatTimestamp/formatTimestamp.ts
+++ src/utils/date/formatTimestamp/formatTimestamp.ts
@@ -7,13 +7,13 @@ export default function formatTimestamp(unixTimestamp: number): string {
         "July", "August", "September", "October", "November", "December"
     ]
 
-    const dayName = days[date.getUTCDay()]
-    const day = date.getUTCDate()
-    const month = months[date.getUTCMonth()]
-    const year = date.getUTCFullYear()
+    const dayName = days[date.getDay()]
+    const day = date.getDate()
+    const month = months[date.getMonth()]
+    const year = date.getFullYear()
 
-    const hours = date.getUTCHours().toString().padStart(2, '0')
-    const minutes = date.getUTCMinutes().toString().padStart(2, '0')
+    const hours = date.getHours().toString().padStart(2, '0')
+    const minutes = date.getMinutes().toString().padStart(2, '0')
 
     const ordinalSuffix = (n: number) => {
         if (n > 3 && n < 21) {
