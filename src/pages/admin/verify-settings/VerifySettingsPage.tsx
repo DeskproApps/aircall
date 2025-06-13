b0131bbda1e797f9ced1d6ffad7b637988bc20ae
OJ Abba <phavorabba@gmail.com>
2025-06-13 14:10:17 +0100

    [Refactor] Update verify settings appearance to include icons

--- src/pages/admin/verify-settings/VerifySettingsPage.tsx
+++ src/pages/admin/verify-settings/VerifySettingsPage.tsx
@@ -1,6 +1,8 @@
-import { useDeskproLatestAppContext } from "@deskpro/app-sdk";
-import { Button, Stack, TSpan } from "@deskpro/deskpro-ui";
+import { Button, Stack } from "@deskpro/deskpro-ui";
+import { CredentialStatus } from "./components/CredentialStatus";
+import { useDeskproAppTheme, useDeskproLatestAppContext } from "@deskpro/app-sdk";
 import useVerifySettings from "./hooks/useVerifySettings";
+
 
 interface AdminDrawerSettings {
     aircall_api_id?: string
@@ -9,15 +11,13 @@ interface AdminDrawerSettings {
 
 export default function VerifySettingsPage(): JSX.Element {
     const { context } = useDeskproLatestAppContext<unknown, AdminDrawerSettings>()
+    const { theme } = useDeskproAppTheme()
     const settings = context?.settings
 
     const { isLoading, isValid, onVerifyClick } = useVerifySettings({
         aircallApiId: settings?.aircall_api_id,
         aircallApiToken: settings?.aircall_api_token,
     })
-
-    const errorMessage = "Failed verifying settings."
-    const successMessage = "Settings verified successfully."
 
     const isIncompleteSettings = !settings?.aircall_api_id?.trim() || !settings?.aircall_api_token?.trim()
 
@@ -27,15 +27,10 @@ export default function VerifySettingsPage(): JSX.Element {
                 text="Verify Settings"
                 intent="secondary"
                 disabled={isIncompleteSettings || isLoading}
-                loading={isLoading}
                 onClick={onVerifyClick}
             />
 
-            {isValid !== undefined &&
-                (
-                    <TSpan> {isValid ? successMessage : errorMessage}</TSpan>
-                )
-            }
+            <CredentialStatus theme={theme} isLoading={isLoading} isValid={isValid} />
         </Stack>
     )
 }
